79e0fcd9dd71619d3ba128c30a997ed89c081a04
CoyotWilly <coyotwilly@gmail.com>
2023-08-07 18:56:52 +0200

    Dropdown lists styling

--- unity/kuka-ar-unity/Assets/Project/Scripts/Connectivity/Temp/AddRobotRequest.cs
+++ unity/kuka-ar-unity/Assets/Project/Scripts/Connectivity/Temp/AddRobotRequest.cs
@@ -3,6 +3,7 @@ namespace Project.Scripts.Connectivity.Temp
     public class AddRobotRequest
     {
         public string IpAddress { get; set; }
+        public string RobotCategory { get; set; }
         public string RobotName { get; set; }
     }
 }
--- unity/kuka-ar-unity/Assets/Project/Scripts/EventSystem/Controllers/AddRobotController.cs
+++ unity/kuka-ar-unity/Assets/Project/Scripts/EventSystem/Controllers/AddRobotController.cs
@@ -24,6 +24,7 @@ public class AddRobotController : MonoBehaviour
         Request = new AddRobotRequest
         {
             IpAddress = null,
+            RobotCategory = null,
             RobotName = null
         };
 
