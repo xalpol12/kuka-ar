04e76682def55c1c148019d5f81c08a4f71fa0be
CoyotWilly <coyotwilly@gmail.com>
2023-08-17 19:10:44 +0200

    FPS controller

--- unity/kuka-ar-unity/Assets/Project/Scripts/EventSystem/Controllers/FrameRateController.cs
+++ unity/kuka-ar-unity/Assets/Project/Scripts/EventSystem/Controllers/FrameRateController.cs
@@ -6,16 +6,14 @@ using UnityEngine.Serialization;
 
 public class FrameRateController : MonoBehaviour
 {
-    public static FrameRateController frameRate; 
-    
     [Header("Frame settings")] 
     private const int MaxRate = 9999;
-    internal float targetFrameRate = 60.0f;
+    [SerializeField]
+    private float targetFrameRate = 60.0f;
     private float currentFrameTime;
 
     private void Awake()
     {
-        frameRate = this;
         QualitySettings.vSyncCount = 0;
         Application.targetFrameRate = MaxRate;
         currentFrameTime = Time.realtimeSinceStartup;
