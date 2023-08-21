b5bf1218166c43c5b7536d54cc72b006641675cf
CoyotWilly <coyotwilly@gmail.com>
2023-08-21 19:02:08 +0200

    Loading screen init

---
+++ unity/kuka-ar-unity/Assets/Project/Scripts/EventSystem/Controllers/LoadingScreen/ASyncLoader.cs
@@ -0,0 +1,32 @@
+
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ASyncLoader : MonoBehaviour
+{
+    [Header("Menu Screens")]
+    [SerializeField] private GameObject loadingScreen;
+    
+    [Header("Slider")]
+    [SerializeField] private Slider progressbar;
+
+    private void Start()
+    {
+        loadingScreen.SetActive(true);
+        
+        StartCoroutine(LoadLevelAsync("MainScene"));
+    }
+
+    IEnumerator LoadLevelAsync(string level)
+    {
+        var loadOperation = SceneManager.LoadSceneAsync(level);
+
+        while (!loadOperation.isDone)
+        {
+            progressbar.value = Mathf.Clamp(loadOperation.progress / 0.9f, 0,1);
+            yield return null;
+        }
+    }
+}
