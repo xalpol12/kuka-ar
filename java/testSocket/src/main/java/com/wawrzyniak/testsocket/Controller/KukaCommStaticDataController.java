c7623a58deb546eb2e189ce6cef4b1c067328108
pWaw <przemko_wawrzyniak@wp.pl>
2023-08-22 12:53:04 +0200

    Mocked db added to testsocket + ip path variable deleted from kukaComm

--- java/testSocket/src/main/java/com/wawrzyniak/testsocket/Controller/KukaCommStaticDataController.java
+++ java/testSocket/src/main/java/com/wawrzyniak/testsocket/Controller/KukaCommStaticDataController.java
@@ -1,15 +1,19 @@
 package com.wawrzyniak.testsocket.Controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.wawrzyniak.testsocket.Exceptions.RobotNotConfiguredException;
+import com.wawrzyniak.testsocket.Model.ModelReading.ConfiguredRobotDTO;
 import com.wawrzyniak.testsocket.Model.Records.RobotData;
 import com.wawrzyniak.testsocket.Model.Value.KRLValue;
 import com.wawrzyniak.testsocket.Model.ValueSetRequest;
+import com.wawrzyniak.testsocket.Service.ConfiguredRobotService;
 import com.wawrzyniak.testsocket.Service.ImageService;
 import com.wawrzyniak.testsocket.Service.KukaMockService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -19,10 +23,13 @@ public class KukaCommStaticDataController {
     private final KukaMockService kukaService;
     private final ImageService imageService;
 
+    private final ConfiguredRobotService robotService;
+
     @Autowired
-    KukaCommStaticDataController(KukaMockService service, ImageService imageService){
+    KukaCommStaticDataController(KukaMockService service, ImageService imageService, ConfiguredRobotService robotService){
         kukaService = service;
         this.imageService = imageService;
+        this.robotService = robotService;
     }
 
     @GetMapping("configured")
@@ -34,6 +41,31 @@ public class KukaCommStaticDataController {
     public Map<String, byte[]> getAllStickers() throws IOException {
         return imageService.getAllStickers();
     }
+
+    @GetMapping("robots")
+    public List<ConfiguredRobotDTO> getALLRobotsWithStickers() {
+
+        return robotService.getAllConfiguredRobots();
+    }
+    @GetMapping("robot/{ip}")
+    public ConfiguredRobotDTO getRobotByIp(@PathVariable String ip) throws RobotNotConfiguredException {
+        return robotService.getRobotByIp(ip);
+    }
+
+    @PostMapping("add")
+    public ConfiguredRobotDTO addRobot(@RequestBody ConfiguredRobotDTO robotDTO) {
+        return robotService.save(robotDTO);
+    }
+    @PostMapping("update")
+    public ConfiguredRobotDTO updateRobot(@RequestBody ConfiguredRobotDTO robotDTO) throws RobotNotConfiguredException {
+        return robotService.updateByIp(robotDTO);
+    }
+
+    @DeleteMapping("delete/{ip}")
+    public void deleteRobot(@PathVariable String ip) {
+        robotService.deleteByIp(ip);
+    }
+
 
     @PostMapping("random")
     public boolean isRandomizing(@RequestBody boolean setRandomizing){
