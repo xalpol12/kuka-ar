3efa0d27d8a9382a4b618faaaa067d3ee0c23737
CoyotWilly <coyotwilly@gmail.com>
2023-09-11 18:14:26 +0200

    Web communication for PUT method, robot update related stuff

--- unity/kuka-ar-unity/Assets/Project/Scripts/Connectivity/Http/Requests/PostNewRobotRequest.cs
+++ unity/kuka-ar-unity/Assets/Project/Scripts/Connectivity/Http/Requests/PostNewRobotRequest.cs
@@ -1,4 +1,3 @@
-using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,19 +9,18 @@ namespace Project.Scripts.Connectivity.Http.Requests
     public class PostNewRobotRequest : IHttpRequest<object>
     {
         private static string URL => "/add";
-        private readonly Robot newRobot;
+        private readonly Robot robot;
         
         public PostNewRobotRequest(Robot robot)
         {
-            newRobot = robot;
+            this.robot = robot;
         }
         
         public async Task<object> Execute(HttpClient httpClient)
         {
             var stringContent = new StringContent(
-                JsonConvert.SerializeObject(newRobot), Encoding.UTF8, "application/json");
-            await httpClient.PostAsync(httpClient.BaseAddress + URL, stringContent);
-            return HttpStatusCode.OK;
+                JsonConvert.SerializeObject(robot), Encoding.UTF8, "application/json"); 
+            return await httpClient.PostAsync(httpClient.BaseAddress + URL, stringContent);
         }
     }
 }
