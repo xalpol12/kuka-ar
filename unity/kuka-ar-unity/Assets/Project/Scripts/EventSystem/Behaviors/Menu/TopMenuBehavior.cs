9e6e33f9f543bf0ce96324e2556652b2c7ea1ea4
Coyot Willy <coyotwilly@gmail.com>
2023-10-05 16:22:55 +0200

    Top menu logic files init

---
+++ unity/kuka-ar-unity/Assets/Project/Scripts/EventSystem/Behaviors/Menu/TopMenuBehavior.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace Project.Scripts.EventSystem.Behaviors.Menu
+{
+    public class TopMenuBehavior : MonoBehaviour
+    {
+        private void Start()
+        {
+        
+        }
+
+        private void Update()
+        {
+        
+        }
+    }
+}
