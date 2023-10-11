a65799009e36cadd88daeef00567ae621e1533c8
Coyot Willy <coyotwilly@gmail.com>
2023-10-11 17:42:02 +0200

    Expandable part of top menu remove

--- unity/kuka-ar-unity/Assets/Project/Scripts/EventSystem/Controllers/Menu/TopMenuController.cs
+++ unity/kuka-ar-unity/Assets/Project/Scripts/EventSystem/Controllers/Menu/TopMenuController.cs
@@ -1,7 +1,4 @@
-using System;
 using Project.Scripts.Connectivity.Models.KRLValues;
-using Project.Scripts.EventSystem.Enums;
-using Project.Scripts.EventSystem.Events;
 using Project.Scripts.TrackedRobots;
 using TMPro;
 using UnityEngine;
@@ -10,31 +7,13 @@ namespace Project.Scripts.EventSystem.Controllers.Menu
 {
     public class TopMenuController : MonoBehaviour
     {
-        [Tooltip("Id of event controller")]
-        public int id;
-        
-        [Tooltip("Animation speed")]
-        public int transformFactor;
-        
-        [Tooltip("Menu drop height, counted from screen height [%]")]
-        public float dropScreenHeight;
-        
-        [Tooltip("Jogs component reference object")]
-        public GameObject jogs;
-
-        [NonSerialized] public GameObject TopMenu;
-
-        [NonSerialized] public LogicStates ConstantPanelState;
-
-        [NonSerialized] public LogicStates CoordListState;
-
         [SerializeField]
         [Tooltip("Tracked robots handler reference game object")]
         private GameObject robotModel;
 
         private TrackedRobotsHandler trackedRobot;
-        private TMP_Text toolNo;
-        private TMP_Text baseNo;
+        private TMP_Text tool;
+        private TMP_Text world;
 
         private void Awake()
         {
@@ -43,58 +22,27 @@ namespace Project.Scripts.EventSystem.Controllers.Menu
 
         private void Start()
         {
-            TopMenu = GetComponent<Transform>().gameObject;
-            var constantTopPanel = TopMenu.GetComponent<RectTransform>()
+            var topMenu = GetComponent<Transform>().gameObject;
+            var constantTopPanel = topMenu.GetComponent<RectTransform>()
                 .Find("ConstantInfoPanel").GetComponent<RectTransform>().gameObject;
             
-            toolNo = constantTopPanel.transform.Find("ToolBean").GetComponent<RectTransform>().gameObject
+            tool = constantTopPanel.transform.Find("ToolBean").GetComponent<RectTransform>().gameObject
                 .transform.Find("Tool").GetComponent<TMP_Text>();
-            baseNo = constantTopPanel.transform.Find("WorldBean").GetComponent<RectTransform>().gameObject
+            world = constantTopPanel.transform.Find("WorldBean").GetComponent<RectTransform>().gameObject
                 .transform.Find("World").GetComponent<TMP_Text>();
-
-            ConstantPanelState = LogicStates.Waiting;
-            CoordListState = LogicStates.Waiting;
-            
-            TopMenuEvents.Events.OnBeanClick += OnToolOrBaseClick;
-            TopMenuEvents.Events.OnDragTopMenuSlider += OnMenuDrag;
-            TopMenuEvents.Events.OnDropTopMenuSlider += OnMenuDrop;
             
             trackedRobot.ActiveBaseUpdated += OnBaseValueChange;
             trackedRobot.ActiveToolUpdated += OnToolValueChange;
         }
 
-        private void OnToolOrBaseClick(int uid)
-        {
-            if (id == uid)
-            {
-                ConstantPanelState = LogicStates.Running;
-            }
-        }
-
-        private void OnMenuDrag(int uid)
-        {
-            if (id == uid)
-            {
-                CoordListState = LogicStates.Running;
-            }
-        }
-
-        private void OnMenuDrop(int uid)
-        {
-            if (id == uid)
-            {
-                CoordListState = LogicStates.AutoPulling;
-            }
-        }
-
         private void OnBaseValueChange(object _, KRLInt e)
         {
-            baseNo.text = "Base\n" + e.Value;
+            world.text = "World\n" + e.Value;
         }
 
         private void OnToolValueChange(object _, KRLInt e)
         {
-            toolNo.text = "Tool\n" + e.Value;
+            tool.text = "Tool\n" + e.Value;
         }
     }
 }
